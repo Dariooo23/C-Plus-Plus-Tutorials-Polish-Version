ff8128a01f65cdf6f66fb11963e5e629013eb827
Dario <93215540+Dariooo23@users.noreply.github.com>
2023-05-12 21:51:53 +0200

    Add files via upload

---
+++ CPlusPlusPoradniki/CPlusPlusTutorial1.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h> // <---- Biblioteki dołączane do programu
+
+using namespace std; // <---- Przestrzeń nazw
+
+void funkcja1(int liczba){ // <---- Przykładowa funkcja
+
+    return;
+}
+int funkcja2(string slowo, int liczba){ // <---- Przykładowa funkcja
+
+    return liczba-10;
+}
+string funkcja3(char znak, long double liczba){ // <---- Przykładowa funkcja
+    string slowo;
+
+    return slowo;
+}
+
+int main() // <---- Funkcja main od której zaczyna się cały program
+{
+    bool liczba0=0; //Wartości 0 lub 1
+    int liczba1=0; //Wartości od -2147483648 do 2147483647 w liczbach całkowitych
+    unsigned int liczba2; //Wartości od 0 do 4294967295 w liczbach całkowitych
+    long long int liczba3=0; // Wartości od -9223372036854775808 do 9223372036854775807 w liczbach całkowitych
+    float liczba4=0; // Wartości od 1.175494351*10^-38 do 3.402823466*10^38 w liczbach rzeczywistych
+    double liczba5=0; // Wartości od 2.2250738585072014*10^-308 do 1.7976931348623158*10^308 w liczbach rzeczywistych
+    long double liczba6=0; // Wartości od 2.2250738585072014*10^-308 do 1.7976931348623158*10^308 w liczbach rzeczywistych
+    char znak='a'; // Jedna wartość z tablicy ASCII
+    string slowo="abcd"; // Wiele wartości z tablicy ASCII
+
+    return 0;
+}
+ 
