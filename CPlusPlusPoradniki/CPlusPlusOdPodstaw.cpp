8581519661fdc1251da4d9ab138406a2b973e322
Dario <93215540+Dariooo23@users.noreply.github.com>
2023-05-22 17:40:58 +0200

    Update CPlusPlusOdPodstaw.cpp

--- CPlusPlusPoradniki/CPlusPlusOdPodstaw.cpp
+++ CPlusPlusPoradniki/CPlusPlusOdPodstaw.cpp
@@ -18,7 +18,10 @@ string funkcja3(char znak, long double liczba){ // <---- Przykładowa funkcja
 }
 
 int main() // <---- Funkcja main od której zaczyna się cały program
-{
+{   
+    //W kodzie pojawia się "cout" oraz "endl", pierwsze z nich służy do wypisania dowolnej rzeczy w konsoli, natomiast drugie służy za znak nowej lini
+    //Czyli jeżeli napiszemy "cout<<3;" to w konsoli otrzymamy "3", a jeżeli napiszemy "cout<<3<<endl;" to otrzymamy "3" oraz nasz kursor przeskoczy do nowej lini;
+    
     bool liczba0=0; //Wartości 0 lub 1
     int liczba1=0; //Wartości od -2147483648 do 2147483647 w liczbach całkowitych
     unsigned int liczba2; //Wartości od 0 do 4294967295 w liczbach całkowitych
@@ -29,6 +32,15 @@ int main() // <---- Funkcja main od której zaczyna się cały program
     char znak='a'; // Jedna wartość z tablicy ASCII
     string slowo="abcd"; // Wiele wartości z tablicy ASCII
 
+    
+    //Jeżeli chcemy wywołać jakąś funkcję wystarczy że napiszemy jej nazwę oraz wprowadzimy potrzebne zmienne
+    funkcja1(10);
+    
+    int a=funkcja2("abcd",25);
+    cout<<a<<endl;
+    
+    cout<<funkcja3('z',4.5)<<endl;
+    
     return 0;
 }
  
